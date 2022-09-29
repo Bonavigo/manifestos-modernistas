7749d20d0d84f4afd82b728640ce63e5a7fe7b09
Bruno Bonavigo <71879062+Bonavigo@users.noreply.github.com>
2022-09-29 17:22:27 -0300

    Sem desespero fml
    cheguei em casa

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Fatto con amore da Bruno Bonavigo, 2022
+
+/*fsLightboxInstances['first-lightbox'].open(0);
+fsLightboxInstances['second-lightbox'].props.onOpen = () => console.log('Lightbox pegou!');*/
+
+Waves.attach('.btn-wave');
+Waves.init();
