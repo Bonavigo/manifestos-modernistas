cea8df3b3614012d578360dd44ecb222e3eeaa2a
Bruno Bonavigo <71879062+Bonavigo@users.noreply.github.com>
2022-09-29 18:46:51 -0300

    Terminei
    creio eu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,4 @@
 // Fatto con amore da Bruno Bonavigo, 2022
 
-/*fsLightboxInstances['first-lightbox'].open(0);
-fsLightboxInstances['second-lightbox'].props.onOpen = () => console.log('Lightbox pegou!');*/
-
 Waves.attach('.btn-wave');
 Waves.init();
